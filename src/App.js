73ef7e672a9d52bdd74fab2f58385f6356df4e91
Blessed Zinaka <zinaxybee@gmail.com>
2024-02-12 16:09:56 +0200

    ...

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Box } from '@mui/material';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
+import Feed from './components/Feed';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const app = () => (
+  <BrowserRouter>
+    <Box sx={{ backgroundColor: '#000' }}>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video/:id' element={<VideoDetail />} />
+        <Route path='/channel/:id' element={<ChannelDetail />} />
+        <Route path='/search/:searchTeam' element={<SearchFeed />} />
+      </Routes>
+    </Box>
+  </BrowserRouter>
+);
 
-export default App;
+export default app;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,40 @@
+import { Stack } from '@mui/material';
+import React from 'react';
+import { categories } from '../utils/constants';
+
+const Sidebar = ({ selectedCategory, setSelectedCategory }) => (
+  <Stack
+    direction='row'
+    sx={{
+      overflow: 'auto',
+      height: { sx: 'auto', md: '95%' },
+      flexDirection: { md: 'column' },
+    }}
+  >
+    {categories.map((Category) => (
+      <button
+        key={Category.name}
+        className='category-btn'
+        onClick={() => setSelectedCategory(Category.name)}
+        style={{
+          background: Category.name === selectedCategory && '#fc1501',
+          color: 'white',
+        }}
+      >
+        <span
+          style={{
+            color: Category.name === selectedCategory ? 'white' : 'red',
+            marginRight: '15px',
+          }}
+        >
+          {Category.icon}
+        </span>
+        <span style={{ opacity: Category.name ? '1' : '0.8' }}>
+          {Category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Sidebar;
